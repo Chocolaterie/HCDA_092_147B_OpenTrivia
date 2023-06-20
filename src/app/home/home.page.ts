8b8bd2a7a1311eb9a845654916fb642e2f037f82
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2023-06-20 11:34:12 +0200

    Correction TP

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -19,13 +19,53 @@ export class HomePage {
   // pour teste si erreur
   public haveErrors = false;
 
-  constructor() {}
+  // les réponses possibles
+  public replies = ["Blanc", "Noir", "Marron", "Je ne sais pas"];
 
-  public onSubmit(){
+  // Etat
+  public state = "step_1";
+
+  public displayNextQuestionBtn = false;
+
+  constructor() { }
+
+  /**
+   * Changer d'etat
+   * @param _newState 
+   */
+  public changeState(_newState: string) {
+    this.state = _newState;
+  }
+
+  /**
+   * Tester si l'etat concorde
+   * @param _state 
+   * @returns 
+   */
+  public isState(_state: String): boolean {
+    return this.state == _state;
+  }
+
+  /**
+   * Lors du clic du bouton du formulaire de l'etape 1
+   */
+  public onSubmit() {
     // Contrôle de surface
     // Si erreur
-    if (this.pseudonyme.length < 4){
+    if (this.pseudonyme.length < 4) {
       this.haveErrors = true;
     }
+    else {
+      // je change d'etat (step_2)
+      this.changeState("step_2");
+    }
+  }
+
+  /**
+   * Lors du clic d'un bouton réponse
+   */
+  public onReply() {
+    // Afficher le button suivant
+    this.displayNextQuestionBtn = true;
   }
 }
